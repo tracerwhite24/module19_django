aed59c4aa80604addec8536cddbc5afd0cb46056
tracerwhite <tracerwhite@gmail.com>
2025-01-07 01:56:46 +0300

    Вариант с админкой

---
+++ task1/task1/admin.py
@@ -0,0 +1,34 @@
+from django.contrib import admin
+from .models import Game, Buyer
+
+
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'cost', 'size')
+    list_filter = ('size', 'cost')
+    search_fields = ('title',)
+    list_per_page = 10
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'cost', 'size')
+    list_filter = ('size', 'cost')
+    search_fields = ('title',)
+    list_per_page = 10
+
+
+class BuyerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'balance', 'age')
+    list_filter = ('balance', 'age')
+    search_fields = ('name',)
+    list_per_page = 10
+    readonly_fields = ('balance',)
+
+
+@admin.register(Buyer)
+class BuyerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'balance', 'age')
+    list_filter = ('balance', 'age')
+    search_fields = ('name',)
+    list_per_page = 10
+    readonly_fields = ('balance',)
--- task1/task1/urls.py
+++ task1/task1/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 from .views import sign_up
+from django.contrib import admin
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('platform', views.store_platform, name='platform'),
     path('goods', views.store_goods, name='goods'),
     path('cart', views.store_cart, name='cart'),
